285e45a6494edaca10e9468e7d4203be909a99e8
monikans <138584461+monikans@users.noreply.github.com>
2024-07-23 20:09:03 +0530

    Update lab 4.py

--- lab 4.py
+++ lab 4.py
@@ -11,34 +11,34 @@ plt.tight_layout()
 plt.show()
 
 #Alphabeta
-def minmax(depth,nodeindex,maximize,values,alpha,beta,path):
+def alpha_beta(depth,nodeindex,maximize,values,alpha,beta,path):
   if depth == 3:
     return values[nodeindex], path + [nodeindex]
   if maximize:
-    maxi = float('-inf')
-    max_path = []
+    best = float('-inf')
+    best_path = []
     for i in range(2):
-      val, newpath = minmax(depth+1, nodeindex*2+i, False, values, alpha, beta, path + [nodeindex])
-      if val > maxi:
-        maxi = val
-        max_path = newpath
-        alpha = max(alpha,maxi)
+      val, newpath = alpha_beta(depth+1, nodeindex*2+i, False, values, alpha, beta, path + [nodeindex])
+      if val > best:
+        best = val
+        best_path = newpath
+        alpha = max(alpha,best)
         if alpha >= beta:
           break
-    return maxi, max_path
+    return best, best_path
   else:
-    mini = float('inf')
-    min_path = []
+    best = float('inf')
+    best_path = []
     for i in range(2):
-      val,newpath = minmax(depth+1,nodeindex*2+i,True,values,alpha,beta,path+[nodeindex])
-      if val < mini:
-        mini = val
-        mini_path = newpath
-        beta = min(mini,beta)
+      val,newpath = alpha_beta(depth+1,nodeindex*2+i,True,values,alpha,beta,path+[nodeindex])
+      if val < best:
+        best = val
+        best_path = newpath
+        beta = min(best,beta)
         if alpha >= beta:
           break
-    return mini,mini_path
+    return best,best_path
 values = [3,5,2,9,12,5,23,23]
-optimal,opt_path = minmax(0,0,True,values,float('-inf'),float('inf'),[])
+optimal,opt_path = alpha_beta(0,0,True,values,float('-inf'),float('inf'),[])
 print("optimal value is",optimal)
 print('optimal path is',opt_path)
